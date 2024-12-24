8a2b39103c2d029c3b14b2c829e88fa9115c055e
HyperBeast <89272182+HyperBeastHUB@users.noreply.github.com>
2024-12-24 18:04:17 +0100

    Welcome Text v1.0
    Adds a customizable custom welcome text. You can change how long it will be visible on screen. (SCP SL Exiled v9.0.1)

---
+++ Class1.cs
@@ -0,0 +1,25 @@
+﻿using Exiled.API.Features;
+using Exiled.Events.EventArgs.Player;
+using Exiled.Loader;
+using player = Exiled.Events.Handlers.Player;
+
+namespace scp_sl
+{
+    public class Class1 : Plugin<config>
+    {
+        public static Class1 Instance;
+        public override void OnEnabled()
+        {
+            Instance = this;
+            player.Verified += EventHandlers.Verified.OnVerified;
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            Instance = null;
+            player.Verified -= EventHandlers.Verified.OnVerified;
+            base.OnDisabled();
+        }
+    }
+}
---
+++ config.cs
@@ -0,0 +1,15 @@
+﻿using Exiled.API.Interfaces;
+using System.ComponentModel;
+
+namespace scp_sl
+{
+    public class config : IConfig
+    {
+        [Description("Set the value to true or false whether you want the plugin to be enabled or not.")]
+        public bool IsEnabled { get; set; } = true;
+        [Description("Set the value to true or false whether you want the plugin to be debugged or not.")]
+        public bool Debug { get; set; }
+        [Description("Type in length of text appearing on screen. Default value is set to 3.")]
+        public int Length { get; set; } = 3;
+    }
+}
