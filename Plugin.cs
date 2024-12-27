3a87bb9aa3b11b58ebaaab239952e1eac8800eaa
MyMQL <75244945+MyMQL@users.noreply.github.com>
2024-12-27 18:53:12 +0100

    Rewritten full code and added rainbow feature

---
+++ Plugin.cs
@@ -0,0 +1,30 @@
+﻿using Exiled.API.Features;
+using player = Exiled.Events.Handlers.Player;
+using System;
+
+namespace WelcomeMessage
+{
+    public class Plugin : Plugin<Config>
+    {
+        public static Plugin Instance { get; private set; }
+
+        public override string Author => "HyperBeast"; 
+        public override Version Version => new Version(1, 3, 0);
+        public override string Name => "WelcomeText";  
+        public override Version RequiredExiledVersion => new Version(9, 1, 0);
+
+        public override void OnEnabled()
+        {
+            Instance = this;
+            player.Verified += EventHandlers.Verified.OnVerified;
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            player.Verified -= EventHandlers.Verified.OnVerified;
+            Instance = null;
+            base.OnDisabled();
+        }
+    }
+}
