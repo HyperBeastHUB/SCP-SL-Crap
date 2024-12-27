13a693cc3c75b17a7fbc8604a49bce002fe7b4ea
HyperBeast <89272182+HyperBeastHUB@users.noreply.github.com>
2024-12-27 16:57:48 +0100

    Add files via upload

---
+++ Verified.cs
@@ -0,0 +1,17 @@
+﻿using Exiled.Events.EventArgs.Player;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace scp_sl.EventHandlers
+{
+    public static class Verified
+    {    
+        public static void OnVerified(VerifiedEventArgs ev)
+        {
+            ev.Player.ShowHint("Welcome to the server?", Class1.Instance.Config.Length);
+        }
+    }
+}
